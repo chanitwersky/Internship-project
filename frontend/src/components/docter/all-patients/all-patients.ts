45498fbf0cebce65b3da25006592516b2bd0ed1f
Esti Stefansky <Hand1e19@mbjcomp.org.il>
2026-04-29 13:46:17 +0300

    fix: cleaned up git cache and applied gitignore

--- frontend/src/components/docter/all-patients/all-patients.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-all-patients',
-  imports: [],
-  templateUrl: './all-patients.html',
-  styleUrl: './all-patients.css',
-})
-export class AllPatients {}
--- frontend/src/components/patient/change-details/change-details.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-change-details',
-  imports: [],
-  templateUrl: './change-details.html',
-  styleUrl: './change-details.css',
-})
-export class ChangeDetails {}
