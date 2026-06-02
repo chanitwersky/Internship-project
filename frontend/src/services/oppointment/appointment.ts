975e5ecd3deb8426ca80aa0842b24de4d62571ea
chanitwersky <hand1e12@mbjcomp.org.il>
2026-06-02 23:43:02 +0300

    fiiling appointment

---
+++ frontend/src/services/oppointment/appointment.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Appointment {
+
+  private apiUrl = 'https://localhost:3001/api';
+
+  constructor(private http: HttpClient) {}
+
+  updateDescription(id: string, description: string): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/update-description/${id}`,  description );
+  }
+
+  getAppointmentById(id: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/appointments/${id}`);
+  }
+
+  finishAppointment(id: string): Observable<any> {
+
+  return this.http.post(
+    `${this.apiUrl}/finish/${id}`,
+    {}
+  );
+}
+
+}
