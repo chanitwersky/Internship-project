47be6eeae98bb80ad25e2d7b4502341f955b5a5b
Tamar Abramowitz <t0533172467@gmail.com>
2026-06-08 20:28:00 +0300

    feat: save my work before merging main

---
+++ backend/Bl/Api/DoctorBlApi.cs
@@ -0,0 +1,20 @@
+﻿using Dal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl.Api
+{
+    public interface IDoctorBl
+    {
+        Task<List<Customer>> GetAllpait(string doctorId);
+        Task<List<Shift>> GetShiftsByDoctorId(string doctorId);
+        Task UpdateAppointment(string treatmentDescription, string id);
+        Task<Queue> GetAppointmentByAppointmentId(string appointmentId);
+        //Task<bool> DeleteOppointmentByAppointmentId(string appointmentId)
+        Task FinishAppointment(string appointmentId);
+
+    }
+}
---
+++ backend/Bl/Services/DoctorService.cs
@@ -0,0 +1,50 @@
+﻿using Bl.Api;
+using Dal.Models;
+using Dal.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl.Services
+{
+    public class DoctorService: IDoctorBl
+    {
+        private readonly DoctorDal _DoctorService;
+
+        public DoctorService(DoctorDal doctorService)
+        {
+            _DoctorService = doctorService;
+        }
+
+        public async Task<List<Customer>> GetAllpait(string doctorId)
+        {
+            return await _DoctorService.GetAllpait(doctorId);
+        }
+
+        public async Task<List<Shift>> GetShiftsByDoctorId(string doctorId)
+        {
+            return await _DoctorService.GetShiftsByDoctorId(doctorId);
+        }
+
+        public async Task UpdateAppointment(string treatmentDescription, string id)
+        {
+            await _DoctorService.UpdateAppointment(treatmentDescription,id);
+        }
+
+        public async Task<Queue> GetAppointmentByAppointmentId(string appointmentId)
+        {
+            return await _DoctorService.GetAppointmentByAppointmentId(appointmentId);
+        }
+
+        //public async Task<bool> DeleteOppointmentByAppointmentId(string appointmentId)
+        //{
+        //    return await _DoctorService.DeleteOppointmentByAppointmentId(appointmentId);
+        //}
+        public async Task FinishAppointment(string appointmentId)
+        {
+            await _DoctorService.FinishAppointment(appointmentId);
+        }
+    }
+}
